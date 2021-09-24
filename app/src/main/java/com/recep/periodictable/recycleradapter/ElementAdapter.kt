73b880e2a27264263a36d46d3399342eae78275f
Recep Fırıncıoğlu <58929094+recfr@users.noreply.github.com>
2021-09-24 11:34:26 +0300

    adapter item data binding added

--- app/src/main/java/com/recep/periodictable/recycleradapter/ElementAdapter.kt
+++ app/src/main/java/com/recep/periodictable/recycleradapter/ElementAdapter.kt
@@ -6,13 +6,16 @@ import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.recep.periodictable.R
 import com.recep.periodictable.model.ElementModel
+import com.recep.periodictable.databinding.AdapterItemElementBinding
 
 class ElementAdapter(private val elementList: List<ElementModel>, private val supportFragmentManager: FragmentManager) :
     RecyclerView.Adapter<ElementViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElementViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.layout.adapter_item_element, parent, false)
-        return ElementViewHolder(itemView, supportFragmentManager, elementList)
+//        val itemView = inflater.inflate(R.layout.adapter_item_element, parent, false)
+//        return ElementViewHolder(itemView, supportFragmentManager, elementList)
+        val binding = AdapterItemElementBinding.inflate(inflater, parent, false)
+        return ElementViewHolder(binding,supportFragmentManager, elementList)
     }
 
     override fun onBindViewHolder(holder: ElementViewHolder, position: Int) {
--- app/src/main/java/com/recep/periodictable/recycleradapter/ElementViewHolder.kt
+++ app/src/main/java/com/recep/periodictable/recycleradapter/ElementViewHolder.kt
@@ -6,13 +6,18 @@ import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.recep.periodictable.R
+import com.recep.periodictable.databinding.AdapterItemElementBinding
 import com.recep.periodictable.model.ElementModel
 import com.recep.periodictable.enum.ElementType
 import com.recep.periodictable.ui.ElementDialogFragment
 
 
-class ElementViewHolder(itemView: View, private val dialog: FragmentManager, model: List<ElementModel>) :
-    RecyclerView.ViewHolder(itemView) {
+class ElementViewHolder(
+    private val binding: AdapterItemElementBinding,
+    private val dialog: FragmentManager,
+    model: List<ElementModel>
+) :
+    RecyclerView.ViewHolder(binding.root) {
     init {
         itemView.setOnClickListener {
             createDialog(model)
@@ -35,20 +40,10 @@ class ElementViewHolder(itemView: View, private val dialog: FragmentManager, mod
     }
 
     fun bind(element: ElementModel) {
-        val elementAtomNumber = itemView.findViewById<TextView>(R.id.textView_atom_number)
-        val elementSymbol = itemView.findViewById<TextView>(R.id.textView_symbol)
-        val elementName = itemView.findViewById<TextView>(R.id.textView_name)
-        val elementType = itemView.findViewById<FrameLayout>(R.id.element_type)
-        val beautification = itemView.findViewById<FrameLayout>(R.id.beautification)
-
-        elementAtomNumber.text = element.atomNumber
-        elementSymbol.text = element.symbol
-        elementName.text = element.name
-
-        beautification.backgroundConfig(element)
-        elementType.setIndicator(element)
+        binding.element = element
+        binding.beautification.backgroundConfig(element)
+        binding.elementType.setIndicator(element)
         itemView.hideItem(element)
-
     }
 
     private fun View.backgroundConfig(element: ElementModel) {
@@ -94,6 +89,5 @@ class ElementViewHolder(itemView: View, private val dialog: FragmentManager, mod
             this.visibility = View.GONE
         }
     }
-
 
 }
